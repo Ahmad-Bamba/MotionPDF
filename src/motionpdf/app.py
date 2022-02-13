17c69f714d845df065db4de2edd316c82ae9a593
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2022-02-13 01:54:08 -0600

    Working 0.0.1

--- src/motionpdf/app.py
+++ src/motionpdf/app.py
@@ -6,7 +6,7 @@ import glob
 from motion import convertPDF
 
 
-def curate_pdfs(path: Path, o: str, directory: bool, 
+def curate_pdfs(path: Path, o: str, 
                 lang: str, lineS: int, verbose: bool) -> None:
     """Find the PDF files the user specified and send them to be converted"""
     o_path = None
@@ -18,8 +18,15 @@ def curate_pdfs(path: Path, o: str, directory: bool,
         
     n = 0
 
+    directory = path.is_dir()
     if directory:
-        n = convertPDF(list(glob.glob("*.pdf")) + list(glob.glob("*.PDF")))
+        print(path)
+        search_pdfs = list(glob.glob(str(path) + "/*.pdf")) \
+            + list(glob.glob(str(path) + "/*.PDF"))
+        search_pdfs = [Path(p) for p in search_pdfs]
+        print(search_pdfs)
+        n = convertPDF(search_pdfs,
+                       o_path, lang, lineS, verbose)
     else:
         if path.suffix.lower() == '.pdf':
             n = convertPDF([path], o_path, lang, lineS, verbose)
@@ -43,20 +50,24 @@ def main():
     parser = argparse.ArgumentParser(description="A script built on "
                                     "Tesseract-OCR for converting .pdf to .txt")
     parser.add_argument('path', help="Path to PDF file(s).", type=is_path)
-    parser.add_argument('--directory', '-d', action='store_true',
-                    help="Use if supplied path is directory of PDF files")
     parser.add_argument('--verbose', '-v', action='store_true', 
                     help="Print debug information")
     parser.add_argument('--out', '-o', help="Directory to put the resulting "
-                    ".txt file(s). Default: [currect directory]/text", 
+                    ".txt file(s). Default: [currect directory]", 
                     type=str, default=os.getcwd(), required=False)
     parser.add_argument('--language', '-l', help="Tesseract language code "
                         "(e.g.) 'eng', 'spa', 'fra'. Defaults to 'eng'.",
                         type=str, default='eng', required=False)
-    parser.add_argument('--linewidth', '-L', help="Maximum line size of output "
-                        "text (default is 120 characters).", type=int,
-                        default=120, required=False)
-    parser.parse_args()
+    parser.add_argument('--linewidth', '-L', help="Force output to wrap to "
+                        "sized lines. Use 0 to try to preserve original lines "
+                        "(default is 0)", type=int, default=0, required=False)
+    args = parser.parse_args()
+    print(args)
 
-    curate_pdfs(parser.path, parser.out, parser.directory, 
-                parser.language, parser.linewidth, parser.verbose)
+    curate_pdfs(args.path, args.out,
+                args.language, args.linewidth, args.verbose)
+
+
+# for local debugging only
+if __name__ == '__main__':
+    main()
--- src/motionpdf/motion.py
+++ src/motionpdf/motion.py
@@ -5,6 +5,7 @@ from pdf2image import convert_from_path
 from datetime import datetime
 import textwrap
 import shutil
+import traceback
 try:
     from PIL import Image
 except ImportError:
@@ -22,8 +23,9 @@ def convertPDF(files: List[Path], outdir: Path,
     sucessfully_processed = 0
     try:
         # create image directory
+        now = datetime.now()
         image_dir = Path(outdir / 
-            ("images" + datetime.strftime("%Y%m%d%H%M%S"))) 
+            ("images" + now.strftime("%Y%m%d%H%M%S"))) 
         image_dir.mkdir()
         for pdf in files:
             images = convert_from_path(pdf)
@@ -33,22 +35,29 @@ def convertPDF(files: List[Path], outdir: Path,
             # convert all files to jpegs
             img_paths = [] 
             for i in range(len(images)):
-                imgp = str(this_pdf_dir) + "/page" + str(i) + ".jpg", "JPEG"
-                images[i].save(imgp)
+                imgp = str(this_pdf_dir) + "/page" + str(i) + ".jpg"
+                images[i].save(imgp, "JPEG")
                 img_paths.append(imgp)
             # read text to memory
             text = []
             for i in range(len(img_paths)):
-                page_text = pytesseract.image_to_string(Image.open(img_paths[i]), 
+                image = Image.open(img_paths[i])
+                # double the image size to try and improve accuracy
+                w, h = image.size
+                image = image.resize((w * 2, h * 2), Image.BOX)
+                page_text = pytesseract.image_to_string(image, 
                                                         lang=lang)
                 text.append((i, page_text))
             # write read text to disk
             outfile_path = str(Path(outdir / (str(pdf.stem) + ".txt")))
-            with open(outfile_path, "+x") as outfile:
+            with open(outfile_path, "+w") as outfile:
                 for line in text:
                     outfile.write("Page {}\n".format(line[0]))
-                    wrapped = textwrap.fill(line[1], width=lineS)
-                    outfile.write("{}\n\n".format(wrapped))
+                    if lineS == 0:
+                        outfile.write("{}\n\n".format(line[1]))
+                    else:
+                        wrapped = textwrap.fill(line[1], width=lineS)
+                        outfile.write("{}\n\n".format(wrapped))
             # if not verbose, delete images
             if not verbose:
                 shutil.rmtree(image_dir)
@@ -60,6 +69,7 @@ def convertPDF(files: List[Path], outdir: Path,
             sucessfully_processed += 1
     except Exception as e:
         if verbose:
+            traceback.print_exc()
             print(e.with_traceback)
         else:
             print(e)
